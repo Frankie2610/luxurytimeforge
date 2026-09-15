9da70690ef74ca86aec5582d119311b25ec8fb3d
Quyen Van <119769481+Frankie2610@users.noreply.github.com>
2026-09-15 18:04:09 +0700

    fix: load account final css after account styles

---
+++ src/account-route-v685.ts
@@ -0,0 +1,7 @@
+import * as account from './customer-account-v12';
+import './v685-account-final.css';
+
+export const CustomerLoginV12 = account.CustomerLoginV12;
+export const CustomerAccountV12 = account.CustomerAccountV12;
+export const CustomerOrderV12 = account.CustomerOrderV12;
+export const TrackOrderV12 = account.TrackOrderV12;
